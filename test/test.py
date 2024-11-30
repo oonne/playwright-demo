5dd39e5b764792ac15067cdb677773455c714987
JAY <JAY@oonne.com>
2024-11-30 14:42:06 +0800

    test: 保存登录态

--- test/test.py
+++ test/test.py
@@ -1,5 +1,5 @@
 import unittest
-from unittest.async_case import IsolatedAsyncioTestCase
+from time import sleep
 from playwright.sync_api import sync_playwright
 from playwright.async_api import async_playwright
 
@@ -17,9 +17,9 @@ class TestExample(unittest.TestCase):
         with sync_playwright() as playwright:
             # 创建浏览器上下文，每个上下文是独立的登录环境
             browser = playwright.chromium.launch(headless=False, devtools=True)
-            content = browser.new_context()
-            # 每个 content 就是一个会话窗口，可以创建自己的页面，也就是浏览器上的 tab 栏
-            page = content.new_page()
+            context = browser.new_context()
+            # 每个 context 就是一个会话窗口，可以创建自己的页面，也就是浏览器上的 tab 栏
+            page = context.new_page()
 
             # 页面打开指定网址
             page.goto('https://www.baidu.com')
@@ -31,7 +31,7 @@ class TestExample(unittest.TestCase):
             page.wait_for_timeout(10000)
 
             # 使用完成关闭上下文（也就是会话窗口）
-            content.close()
+            context.close()
             # 关闭浏览器
             browser.close()
 
@@ -49,9 +49,9 @@ class AsyncTestExample(unittest.IsolatedAsyncioTestCase):
         playwright = await async_playwright().start()
         # 创建浏览器上下文，每个上下文是独立的登录环境
         browser = await playwright.chromium.launch(headless=False, devtools=True)
-        content = await browser.new_context()
-        # 每个 content 就是一个会话窗口，可以创建自己的页面，也就是浏览器上的 tab 栏
-        page = await content.new_page()
+        context = await browser.new_context()
+        # 每个 context 就是一个会话窗口，可以创建自己的页面，也就是浏览器上的 tab 栏
+        page = await context.new_page()
 
         # 页面打开指定网址
         await page.goto('https://www.tiktok.com')
@@ -65,7 +65,36 @@ class AsyncTestExample(unittest.IsolatedAsyncioTestCase):
         await page.wait_for_timeout(10000)
 
         # 使用完成关闭上下文（也就是会话窗口）
-        await content.close()
+        await context.close()
+        # 关闭浏览器
+        await browser.close()
+
+    # 测试用例3：测试登录
+    async def test_3(self):
+        playwright = await async_playwright().start()
+        # 创建浏览器上下文，每个上下文是独立的登录环境
+        browser = await playwright.chromium.launch(headless=False, devtools=True)
+        # 尝试加载之前保存的状态
+        try:
+            context = await browser.new_context(storage_state="../data/test_3.json")
+        except FileNotFoundError:
+            context = await browser.new_context()
+        # 每个 context 就是一个会话窗口，可以创建自己的页面，也就是浏览器上的 tab 栏
+        page = await context.new_page()
+
+        # 页面打开指定网址
+        await page.goto('https://link-ai.tech/home')
+
+        # 此处先手动登录，没有做自动化
+
+        # 延迟关闭
+        sleep(20)
+
+        # 保存登录态
+        await context.storage_state(path="../data/test_3.json")
+
+        # 使用完成关闭上下文（也就是会话窗口）
+        await context.close()
         # 关闭浏览器
         await browser.close()
 
