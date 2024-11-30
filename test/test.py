cffde253b02cb5306a959cfbeff0ccbb30bf0b5e
JAY <JAY@oonne.com>
2024-11-30 10:21:46 +0800

    test: 异步执行测试用例

--- test/test.py
+++ test/test.py
@@ -1,5 +1,7 @@
 import unittest
-from playwright.sync_api import Playwright, sync_playwright
+from unittest.async_case import IsolatedAsyncioTestCase
+from playwright.sync_api import sync_playwright
+from playwright.async_api import async_playwright
 
 class TestExample(unittest.TestCase):
     def setUp(self):
@@ -10,7 +12,7 @@ class TestExample(unittest.TestCase):
         # 清理代码
         pass
 
-    # 测试用例1：打开浏览器并搜搜
+    # 测试用例1：同步 打开浏览器并搜索
     def test_1(self):
         with sync_playwright() as playwright:
             # 创建浏览器上下文，每个上下文是独立的登录环境
@@ -33,24 +35,33 @@ class TestExample(unittest.TestCase):
             # 关闭浏览器
             browser.close()
 
-    # 测试用例2：翻墙
-    def test_2(self):
-        with sync_playwright() as playwright:
-            # 创建浏览器上下文，每个上下文是独立的登录环境
-            browser = playwright.chromium.launch(headless=False)
-            content = browser.new_context()
-            # 每个 content 就是一个会话窗口，可以创建自己的页面，也就是浏览器上的 tab 栏
-            page = content.new_page()
+class AsyncTestExample(unittest.IsolatedAsyncioTestCase):
+    def setUp(self):
+        # 初始化代码
+        pass
 
-            # 页面打开指定网址
-            page.goto('https://www.google.com')
-            # 延迟关闭
-            page.wait_for_timeout(10000)
+    def tearDown(self):
+        # 清理代码
+        pass
 
-            # 使用完成关闭上下文（也就是会话窗口）
-            content.close()
-            # 关闭浏览器
-            browser.close()
+    # 测试用例2：获取user_agent
+    async def test_2(self):
+        playwright = await async_playwright().start()
+        # 创建浏览器上下文，每个上下文是独立的登录环境
+        browser = await playwright.chromium.launch(headless=False)
+        content = await browser.new_context()
+        # 每个 content 就是一个会话窗口，可以创建自己的页面，也就是浏览器上的 tab 栏
+        page = await content.new_page()
+
+        # 页面打开指定网址
+        await page.goto('https://tiktok.com')
+        # 延迟关闭
+        await page.wait_for_timeout(10000)
+
+        # 使用完成关闭上下文（也就是会话窗口）
+        await content.close()
+        # 关闭浏览器
+        await browser.close()
 
 if __name__ == '__main__':
     unittest.main()
