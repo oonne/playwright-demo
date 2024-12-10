2d639b6ec40de1ee763cd42bfeba87fcdc6e8143
JAY <JAY@oonne.com>
2024-12-10 18:18:19 +0800

    test: 创建页面

--- tiktok/login.py
+++ tiktok/login.py
@@ -1,11 +1,15 @@
+import asyncio
+import time
+from playwright.async_api import async_playwright
+from utils.browser import create_browser
 
-# 写一个函数
-def login():
+async def login():
     """
     获取Tiktok登录二维码
     """
-    print('test')
+    browser = await create_browser('tiktok')
+    print(browser)
 
 
 if __name__ == '__main__':
-    login()
+    asyncio.run(login())
---
+++ utils/browser.py
@@ -0,0 +1,27 @@
+from playwright.async_api import async_playwright
+import random
+import string
+
+async def create_browser(contextName=None, timezone_id='Asia/Shanghai', locale='zh-CN'):
+    """
+    创建一个浏览器实例，并做指纹处理
+    """
+    if not contextName:
+        contextName = ''.join(random.sample(string.ascii_letters + string.digits, 16))
+
+    playwright = await async_playwright().start()
+    context = await playwright.chromium.launch_persistent_context("../data/"+contextName,
+        headless=False,
+        devtools=True,
+        timezone_id=timezone_id,
+        locale=locale,
+        viewport={'width': 1440, 'height': 900},
+    )
+    page = await context.new_page()
+
+    return {
+        'playwright': playwright,
+        'context': context,
+        'page': page,
+    }
+
